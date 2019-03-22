d0c802677889af79f23e2e122d72591e110f2617
MaxiRGor <maxirgor@gmail.com>
2019-03-22 23:33:24 +0300

    Statistics improved

--- quizwithmoxy/src/main/java/harelchuk/maxim/quizwithmoxy/fragments/SetEmblemFragment.java
+++ quizwithmoxy/src/main/java/harelchuk/maxim/quizwithmoxy/fragments/SetEmblemFragment.java
@@ -4,14 +4,40 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.arellomobile.mvp.MvpAppCompatFragment;
+import com.squareup.picasso.Picasso;
 
 import harelchuk.maxim.quizwithmoxy.R;
 
 public class SetEmblemFragment extends MvpAppCompatFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.set_emblem_fragment, container, false);
+        View view = inflater.inflate(R.layout.set_emblem_fragment, container, false);
+        function(view);
+        return view;
+    }
+
+    private void function(View view) {
+        TextView starksTV = view.findViewById(R.id.emblemUserSkinStarksTV);
+
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final ImageView imageView = getActivity().
+                        findViewById(R.id.imageViewMenu);
+                Picasso.get().
+                        load(R.drawable.background_starks)
+                        .fit()
+                        .placeholder(R.drawable.blackscreen)
+                        .into(imageView);
+            }
+        };
+
+        starksTV.setOnClickListener(onClickListener);
+
+
     }
 }
--- quizwithmoxy/src/main/java/harelchuk/maxim/quizwithmoxy/fragments/StatisticsFragment.java
+++ quizwithmoxy/src/main/java/harelchuk/maxim/quizwithmoxy/fragments/StatisticsFragment.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,6 +17,7 @@ import com.arellomobile.mvp.MvpAppCompatFragment;
 import com.arellomobile.mvp.presenter.InjectPresenter;
 
 import harelchuk.maxim.quizwithmoxy.R;
+import harelchuk.maxim.quizwithmoxy.model.SharedPreferencesFunctions;
 import harelchuk.maxim.quizwithmoxy.presenter.StatisticsPresenter;
 import harelchuk.maxim.quizwithmoxy.view.StatisticsView;
 
@@ -46,7 +48,7 @@ public class StatisticsFragment extends MvpAppCompatFragment implements Statisti
                                boolean is_skin_targar, boolean is_skin_stark, boolean is_skin_lann, boolean is_skin_night,
                                boolean is_credit, long credit_time, long credit_sum,
                                boolean is_debit, long debit_time, long debit_sum) {
-        TextView user_name_TV = statisticsView.findViewById(R.id.userNameTV);
+        EditText user_name_TV = statisticsView.findViewById(R.id.userNameTV);
         ImageView user_read_watch_TV = statisticsView.findViewById(R.id.userReadWatchIV);
         TextView user_GD_temp_TV = statisticsView.findViewById(R.id.userGDTempTV);
         TextView user_GD_all_TV = statisticsView.findViewById(R.id.userGDAllTV);
@@ -64,45 +66,88 @@ public class StatisticsFragment extends MvpAppCompatFragment implements Statisti
         TextView user_skin_stark_TV = statisticsView.findViewById(R.id.userSkinStarksTV);
         TextView user_skin_lann_TV = statisticsView.findViewById(R.id.userSkinLannTV);
         TextView user_skin_night_TV = statisticsView.findViewById(R.id.userSkinNKTV);
+        ImageView userSkinTargImage = statisticsView.findViewById(R.id.userSkinTargIV);
+        ImageView userSkinStarksImage = statisticsView.findViewById(R.id.userSkinStarkIV);
+        ImageView userSkinLannImage = statisticsView.findViewById(R.id.userSkinLannIV);
+        ImageView userSkinNKImage = statisticsView.findViewById(R.id.userSkinNKIV);
         TextView user_debit_value_TV = statisticsView.findViewById(R.id.userDebitValueTV);
         TextView user_credit_value_TV = statisticsView.findViewById(R.id.userCreditValueTV);
         user_name_TV.setText(user_name);
         user_GD_temp_TV.setText(String.valueOf(money_GD_temp));
         user_AD_temp_TV.setText(String.valueOf(money_AD_temp));
         user_CP_temp_TV.setText(String.valueOf(money_CP_temp));
-        user_GD_all_TV.setText(String.valueOf(money_GD_all));
-        user_AD_all_TV.setText(String.valueOf(money_AD_all));
-        user_CP_all_TV.setText(String.valueOf(money_CP_all));
+        //user_GD_all_TV.setText(String.valueOf(money_GD_all));
+        //user_AD_all_TV.setText(String.valueOf(money_AD_all));
+        //user_CP_all_TV.setText(String.valueOf(money_CP_all));
+        user_GD_all_TV.setVisibility(View.INVISIBLE);
+        user_AD_all_TV.setVisibility(View.INVISIBLE);
+        user_CP_all_TV.setVisibility(View.INVISIBLE);
         user_rounds_GD_TV.setText(String.valueOf(number_hard_games));
         user_rounds_AD_TV.setText(String.valueOf(number_medium_games));
         user_rounds_CP_TV.setText(String.valueOf(number_easy_games));
         user_winnings_GD_TV.setText(String.valueOf(number_hard_winnings));
         user_winnings_AD_TV.setText(String.valueOf(number_medium_winnings));
         user_winnings_CP_TV.setText(String.valueOf(number_easy_winnings));
+
+        int current_theme=0;
+
         if(is_skin_targar) {
-            user_skin_targar_TV.setText(getResources().getString(R.string.targariens));
-        } else {
-            user_skin_targar_TV.setText(getResources().getString(R.string.unavailable));
+            //user_skin_targar_TV.setText(getResources().getString(R.string.targariens));
+            userSkinTargImage.setBackground(getResources().getDrawable(R.drawable.ic_logo_dragon_yes));
+            if(current_theme==0){
+                userSkinTargImage.setBackground(getResources().getDrawable(R.drawable.ic_logo_dragon_en));
+            }
+        }else {
+            user_skin_targar_TV.setText(R.string.unavailable);
         }
         if(is_skin_stark) {
-            user_skin_stark_TV.setText(getResources().getString(R.string.starks));
-        } else {
-            user_skin_stark_TV.setText(getResources().getString(R.string.unavailable));
+            userSkinStarksImage.setBackground(getResources().getDrawable(R.drawable.ic_logo_wolf_yes));
+            if(current_theme==1){
+                userSkinStarksImage.setBackground(getResources().getDrawable(R.drawable.ic_logo_wolf_en));
+            }
+        }else {
+            user_skin_stark_TV.setText(R.string.unavailable);
         }
         if(is_skin_lann) {
-            user_skin_lann_TV.setText(getResources().getString(R.string.lannisters));
-        } else {
-            user_skin_lann_TV.setText(getResources().getString(R.string.unavailable));
+            userSkinLannImage.setBackground(getResources().getDrawable(R.drawable.ic_logo_lion_yes));
+            if(current_theme==2){
+                userSkinLannImage.setBackground(getResources().getDrawable(R.drawable.ic_logo_lion_en));
+            }
+        }else{
+            user_skin_lann_TV.setText(R.string.unavailable);
         }
         if(is_skin_night) {
-            user_skin_night_TV.setText(getResources().getString(R.string.nightKing));
-        } else {
-            user_skin_night_TV.setText(getResources().getString(R.string.unavailable));
+            userSkinNKImage.setBackground(getResources().getDrawable(R.drawable.ic_logo_nk_yes));
+            if(current_theme==3){
+                userSkinNKImage.setBackground(getResources().getDrawable(R.drawable.ic_logo_nk_en));
+            }
+        }else{
+            user_skin_night_TV.setText(R.string.unavailable);
         }
         if(is_debit) {
-            user_debit_value_TV.setText(String.valueOf(debit_sum));
+            //user_debit_value_TV.setText(String.valueOf(debit_sum));
+            SharedPreferencesFunctions sharedPreferencesFunctions = new SharedPreferencesFunctions();
+            long coins[] = sharedPreferencesFunctions.coins_GD_AD_CP(debit_sum);
+            ImageView GD = statisticsView.findViewById(R.id.userDebitGDImage);
+            ImageView AD = statisticsView.findViewById(R.id.userDebitADImage);
+            ImageView CP = statisticsView.findViewById(R.id.userDebitCPImage);
+            TextView debitGD = statisticsView.findViewById(R.id.userDebitGDTV);
+            TextView debitAD = statisticsView.findViewById(R.id.userDebitADTV);
+            TextView debitCP = statisticsView.findViewById(R.id.userDebitCPTV);
+            debitGD.setText(String.valueOf((int)coins[0]));
+            debitAD.setText(String.valueOf((int)coins[1]));
+            debitCP.setText(String.valueOf((int)coins[2]));
+            debitGD.setVisibility(View.VISIBLE);
+            debitAD.setVisibility(View.VISIBLE);
+            debitCP.setVisibility(View.VISIBLE);
+            GD.setVisibility(View.VISIBLE);
+            AD.setVisibility(View.VISIBLE);
+            CP.setVisibility(View.VISIBLE);
+
         } else {
             user_debit_value_TV.setText(getResources().getString(R.string.noDebit));
+            user_debit_value_TV.setVisibility(View.VISIBLE);
+
         }
         if(is_credit) {
             user_credit_value_TV.setText(String.valueOf(credit_sum));
